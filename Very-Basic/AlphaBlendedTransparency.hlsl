67c236c9b8f8917e7b41bc38f34f888301a67a81
rasyid kawaii <68007122+Rasyid536@users.noreply.github.com>
2026-01-19 23:57:08 +0700

    Create AlphaBlendedTransparency.hlsl

---
+++ Very-Basic/AlphaBlendedTransparency.hlsl
@@ -0,0 +1,70 @@
+Shader "BasicLearn/AlphaBlendedTransparency"
+{
+    Properties
+    {
+        _BaseColor("BaseColor", Color) = (1, 1, 1, 1)
+        _BaseTexture("Basic Texture", 2D) = "white" {}
+        [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend("Source Blend Mode", Integer) = 5
+        [Enum(UnityEngine.Rendering.BlendMode)] _DstBlend("Destination Blend", Integer) = 10
+    }
+
+    SubShader
+    {
+        Tags 
+        {
+            "RenderPipeline" = "UniversalPipeline" 
+            "RenderType" = "Transparent"
+            "Queue" = "Transparent" 
+        }
+
+        pass 
+        {
+            Blend [_SrcBlend] [_DstBlend]
+            Zwrite Off
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+            CBUFFER_START(UnityPerMaterial)
+            float4 _BaseColor;
+            float4 _BaseTexture_ST;
+            CBUFFER_END
+
+            TEXTURE2D(_BaseTexture);
+            SAMPLER(sampler_BaseTexture);
+
+            struct Attributes
+            {
+                float4 positionOS : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+            struct v2f 
+            {
+                float4 positionCS : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+
+            v2f vert(Attributes v)
+            {
+                v2f o = (v2f)0;
+
+                o.positionCS = TransformObjectToHClip(v.positionOS.xyz);
+                o.uv = TRANSFORM_TEX(v.uv, _BaseTexture);
+                
+                return o;
+            }
+
+            float4 frag(v2f i) : SV_TARGET
+            {
+                float4 textureColor = SAMPLE_TEXTURE2D(_BaseTexture, sampler_BaseTexture, i.uv);
+                return textureColor * _BaseColor;
+            }
+
+
+            ENDHLSL
+        }
+    }
+    
+}
