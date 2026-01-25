0617a2820ec1a8d54a1c089f747f324ddf448cf7
rasyid kawaii <68007122+Rasyid536@users.noreply.github.com>
2026-01-25 17:44:06 +0700

    Create SineWaves.hlsl

---
+++ Very-Basic/SineWaves.hlsl
@@ -0,0 +1,76 @@
+Shader "BasicLearn/SineWaves"
+{
+    Properties
+    {
+        _BaseColor("BaseColor", Color) = (1, 1, 1, 1)
+        _BaseTexture("Basic Texture", 2D) = "white" {}
+        _WaveHeight ("Wave Height", Range(0.0, 1.0)) = 0.25
+        _WaveSpeed("Wave Speed", Range(0.0, 10.0)) = 1.0
+        [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend("Source Blend Mode", Integer) = 5
+        [Enum(UnityEngine.Rendering.BlendMode)] _DstBlend("Destination Blend", Integer) = 10
+    }
+
+    SubShader
+    {
+        Tags 
+        {
+            "RenderPipeline" = "UniversalPipeline" 
+            "RenderType" = "Opaque"
+            "Queue" = "Geometry" 
+        }
+
+        pass 
+        {
+            Blend [_SrcBlend] [_DstBlend]
+            Zwrite Off
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+            CBUFFER_START(UnityPerMaterial)
+            float4 _BaseColor;
+            float4 _BaseTexture_ST;
+            float _WaveHeight;
+            float _WaveSpeed;
+            CBUFFER_END
+
+            TEXTURE2D(_BaseTexture);
+            SAMPLER(sampler_BaseTexture);
+
+            struct Attributes
+            {
+                float4 positionOS : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+            struct v2f 
+            {
+                float4 positionCS : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+
+            v2f vert(Attributes v)
+            {
+                v2f o = (v2f)0;
+                float3 positionWS = TransformObjectToWorld(v.positionOS.xyz);
+
+                float waveHeight = sin(positionWS.x + positionWS.z + _Time.y * _WaveSpeed ) * _WaveHeight;
+                float3 newPositionWS = float3(positionWS.x, positionWS.y + waveHeight, positionWS.z);
+
+                o.positionCS = TransformWorldToHClip(newPositionWS);
+                o.uv = TRANSFORM_TEX(v.uv, _BaseTexture);
+                
+                return o;
+            }
+
+            float4 frag(v2f i) : SV_TARGET
+            {
+                float4 textureColor = SAMPLE_TEXTURE2D(_BaseTexture, sampler_BaseTexture, i.uv);
+                return textureColor * _BaseColor;
+            }
+            ENDHLSL
+        }
+    }
+    
+}
